d19898345738e7493f563fe11bf85146f9990057
jeohmsptcs <137598837+jeohmsptcs@users.noreply.github.com>
2023-06-24 12:40:34 +0530

    Add files via upload

---
+++ akshay.py
@@ -0,0 +1,11 @@
+def fib(n):
+  if n<=1:
+      return n
+      f.append(f[i-1]+f[i-2])
+    print("The fibonacci sequence is:",f)
+    return +(n)
+n=int input("Enter the number")
+print("The fibonacci value is:",fib)(n)
+
+
+   
